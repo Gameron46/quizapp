0d2da0dc1b3fecb0b6a3342ed9294ee61d2e23db
fahmadov01 <faah2685@colorado.edu>
2024-09-18 16:20:40 -0600

    Add question answer functionality

--- app.js
+++ app.js
@@ -1,43 +1,62 @@
 let questionnumber = 0;
+let questions = [];
+
+function fetchJSONData() {
+    fetch('./questions_answers.json')
+        .then((res) => res.json())
+        .then((data) => {
+            questions=data.questions
+        })
+}
+fetchJSONData();
 
 function continueQuestions() {
     questionnumber++;
-    if (questionnumber == 11) {
+    if (questionnumber === questions.length+1) {
         document.getElementById("Quiz").innerHTML = '<h5>Thanks for taking the quiz!</h5>'
     }
     else {
+        let selectionBox = document.createElement('select');
+        selectionBox.id = 'selectionBox';
+        let options = [];
+
+        for (let option in questions[questionnumber-1].options) {
+            options.push(questions[questionnumber-1].options[option]);
+        }
+
+        for (const option of options) {
+            let optionElement = document.createElement('option');
+            optionElement.textContent = option;
+            selectionBox.appendChild(optionElement);
+        }
+
         document.getElementById("questionNum").innerHTML = "Question " + questionnumber;
-        document.getElementById("questionText").innerHTML = "Insert Question Here" + '<br>' + "The answer is " + '<input type="text" class="answerBox">' + "and other answer is " + '<input type="text" class="answerBox">';
+        document.getElementById("questionText").innerHTML = questions[questionnumber-1].question + '<br>';
+        document.getElementById("questionText").appendChild(selectionBox);
         document.getElementById("nextButton").setAttribute("onclick", "nextQuestion()");
         document.getElementById("nextButton").textContent = "Next";
     }
 }
 
 function nextQuestion() {
-    let answer1 = document.getElementsByClassName("answerBox")[0].value;
-    let answer2 = document.getElementsByClassName("answerBox")[1].value;
-    if (answer1 != "" && answer2 != "") {
-        let isCorrect = checkAnswer(answer1, answer2)
+    let answer = document.getElementById("selectionBox").value;
+    let isCorrect = checkAnswer(answer)
         
-        if (isCorrect) {
-            document.getElementById("questionNum").innerHTML = "";
-            document.getElementById("questionText").innerHTML = "Correct!";
-            document.getElementById("nextButton").setAttribute("onclick", "continueQuestions()");
-        }
-        else {
-            document.getElementById("questionNum").innerHTML = "";
-            document.getElementById("questionText").innerHTML = "Incorrect";
-            document.getElementById("nextButton").setAttribute("onclick", "continueQuestions()");
-        }
+    if (isCorrect) {
+        document.getElementById("questionNum").innerHTML = "";
+        document.getElementById("questionText").innerHTML = "Correct";
+        document.getElementById("nextButton").setAttribute("onclick", "continueQuestions()");
     }
-
     else {
-        alert("Please enter an answer!");
+        document.getElementById("questionNum").innerHTML = "";
+        document.getElementById("questionText").innerHTML = "Incorrect";
+        document.getElementById("nextButton").setAttribute("onclick", "continueQuestions()");
     }
 }
 
-function checkAnswer(answer1, answer2) {
-    if (answer1 == "1" && answer2 == "1") {
+function checkAnswer(answer) {
+    let correctAnswer = questions[questionnumber-1].answer;
+    if (answer == correctAnswer) {
         return true;
     }
     else {
