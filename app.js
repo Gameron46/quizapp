ec8260e8c7d3075795672d5292dd577ae33ade85
fahmadov01 <faah2685@colorado.edu>
2024-09-22 12:13:21 -0600

    Add explanations to answers

--- app.js
+++ app.js
@@ -42,23 +42,42 @@ function continueQuestions() {
 function nextQuestion() {
     let answer = document.getElementById("selectionBox").value;
     let isCorrect = checkAnswer(answer)
+    let explanation = questions[questionnumber-1].explanation
+    let explanationText = document.createElement('p')
+    explanationText.innerHTML = '<b>Explanation: </b>' + explanation
+    let images = []
+
+    if ('image_urls' in questions[questionnumber-1]) {
+      for (let image in questions[questionnumber-1].image_urls) {
+        images.push(questions[questionnumber-1].image_urls[image])
+      }
+    }
         
+    document.getElementById("questionNum").innerHTML = "";
     if (isCorrect) {
         numPoints++;
-        document.getElementById("questionNum").innerHTML = "";
         document.getElementById("questionText").innerHTML = "Correct";
-        document.getElementById("nextButton").setAttribute("onclick", "continueQuestions()");
     }
     else {
-        document.getElementById("questionNum").innerHTML = "";
-        document.getElementById("questionText").innerHTML = "Incorrect";
-        document.getElementById("nextButton").setAttribute("onclick", "continueQuestions()");
+        document.getElementById("questionText").innerHTML = 'Incorrect <br> <b>The correct answer is:</b> ';
+        document.getElementById("questionText").innerHTML += questions[questionnumber-1].answer;
     }
+
+    document.getElementById("questionText").appendChild(explanationText);
+    for (let i = 0; i < images.length; i++) {
+        let img = document.createElement('img')
+        let caption = document.createElement('figcaption')
+        img.src = images[i];
+        caption.textContent = "Figure " + (i+1)
+        document.getElementById("questionText").appendChild(img)
+        document.getElementById("questionText").appendChild(caption)
+    }
+    document.getElementById("nextButton").setAttribute("onclick", "continueQuestions()");
 }
 
 function checkAnswer(answer) {
     let correctAnswer = questions[questionnumber-1].answer;
-    if (answer == correctAnswer) {
+    if (answer === correctAnswer) {
         return true;
     }
     else {
